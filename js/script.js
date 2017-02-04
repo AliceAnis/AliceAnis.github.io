39cefc6e42d9054ce620795155e949757454fdc2
Alisa Anisimova <pantha_n@mail.ru>
2017-02-05 00:46:25 +0200

    index

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	$("header h1").animated("fadeInDown", "fadeOutUp");
+		
+
+	$(".animation_1").animated("fadeInLeft", "fadeOutLeft");
+	$(".animation_2").animated("fadeInRight", "fadeOutRight");
+
+});
